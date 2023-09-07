632d63c428562195ed501bb2685c6381acc839b9
Ijhar Ali <127856710+izhar9838@users.noreply.github.com>
2023-09-07 18:26:26 +0530

    Add files via upload

---
+++ PW-Skill_Project_6/Multi-Page Website/Contact/clas.js
@@ -0,0 +1,18 @@
+// let a=157
+// let b= 197
+// c= a&b
+// d= a|b
+// e= a^b
+// console.log(c,d,e)
+//         128 64 32    16  8   4 2 1
+// a 157 = 1   0   0    1   1   1 0  1
+// b 197 = 1   1   0    0   0   1 0  1
+// -----------------------------
+// c a&b = 1   0   0    0   0   1   0   1
+//         128 64  32  16   8   4   2   1
+//         128  0   0   0    0   4   0   1 =>133
+// 
+// wheather a person can vote or not
+// wap to check wheather two numbers are equal or not
+// wap to check wheather a number is perfect square number or Not
+
